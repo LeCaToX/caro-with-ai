2a0cf65ea6d773094ccaaa5e8788c90cada643a7
LeCaToX <70011797+LeCaToX@users.noreply.github.com>
2022-02-22 16:10:23 +0700

    Initial commit

--- js/boardCtrl.js
+++ js/boardCtrl.js
@@ -1,32 +1,11 @@
 var ctrl = {
-	newGame : function() {
-		currGame = new game(16, 16);
-		board.writeBoard();
-	},
-	undo : function() {
-		alert('Chơi với máy đã gà thế này rồi nên không có chơi lại nha! :D :D ');
-	},
-	resign : function() {
-		alert('Bạn đã chịu thua. :D :D');
-		currGame.isGamming = false;
-	},
-	standUp : function() {
-		if (currGame.Turn == X) {
-			var bestMove = {row: 0, col:0};
-			AIthink(X, bestMove);
-			currGame.sq[bestMove.row][bestMove.col] = X;
-			board.sqUpdate(bestMove.row, bestMove.col);
-			referee.checkWin();
-			currGame.Turn = O;
-			currGame.noOfPiece++;
-		} else {
-			var bestMove = {row:0, col:0};
-			AIthink(O, bestMove);
-			currGame.sq[bestMove.row][bestMove.col] = O;
-			board.sqUpdate(bestMove.row, bestMove.col);
-			referee.checkWin();
-			currGame.Turn = X;
-			currGame.noOfPiece++;
-		}
-	}
+    newGame: function () {
+        currGame = new game(14, 20);
+        board.writeBoard();
+    },
+    resign: function () {
+        alert("Bạn đã chịu thua. :D :D");
+        currGame = new game(14, 30);
+        board.writeBoard();
+    },
 };
