05fdc1888dc60729b943b6b9df398f9c3c382004
StewAway <88744368+StewAway@users.noreply.github.com>
2022-03-29 21:03:28 +0700

    Chỉnh size (temp)

--- js/boardCtrl.js
+++ js/boardCtrl.js
@@ -1,11 +1,11 @@
 var ctrl = {
     newGame: function () {
-        currGame = new game(14, 20);
+        currGame = new game(30, 30);
         board.writeBoard();
     },
     resign: function () {
         alert("Bạn đã chịu thua. :D :D");
-        currGame = new game(14, 30);
+        currGame = new game(30, 30);
         board.writeBoard();
     },
 };
